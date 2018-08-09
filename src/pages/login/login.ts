f8b4ef326c0abd0fe3c799688c34e859b284a78e
Mehul <mehul@hyperleaptech.com>
2018-08-09 12:26:11 +0530

    Usinf cordova sms plugin

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
+import { NavController, AlertController, Platform } from 'ionic-angular';
 
 import firebase from 'firebase';
 import { ToastController } from 'ionic-angular';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
 import { Data } from '../../providers/data/data';
 import { HomePage } from '../home/home';
-import { AndroidPermissions } from '../../../node_modules/@ionic-native/android-permissions';
 declare var SMS: any;
 
 @Component({
@@ -15,61 +15,45 @@ declare var SMS: any;
 export class LoginPage {
   public recaptchaVerifier:firebase.auth.RecaptchaVerifier;
   smsSent: boolean = false;
-  messages:any=[];
-  platform: any;
+  windowRef: any;
 
-  constructor(public navCtrl: NavController, public androidPermissions: AndroidPermissions, public alertCtrl: AlertController, public toastCtrl: ToastController, public dat: Data) {}
+  constructor(public navCtrl: NavController, public androidPermissions: AndroidPermissions, public alertCtrl: AlertController, public toastCtrl: ToastController, public dat: Data, public platform: Platform) {}
 
   ionViewDidLoad() {
     this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
   }
 
-  checkPermission()
-  {
-        this.androidPermissions.checkPermission
-        (this.androidPermissions.PERMISSION.READ_SMS).then(
-        success => {
-                  
-            //if permission granted
-            this.ReadSMSList();
-        },
-        err =>{
-                
-            this.androidPermissions.requestPermission
-            (this.androidPermissions.PERMISSION.READ_SMS).
-            then(success=>{
-                this.ReadSMSList();
-            },
-            err=>{
-                alert("cancelled")
-            });
+  ionViewWillEnter()
+{
+
+this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.READ_SMS).then(
+  success => console.log('Permission granted'),
+err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.READ_SMS)
+);
+
+this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.READ_SMS]);
+}
+
+  ionViewDidEnter() {
+
+    this.platform.ready().then((readySource) => {
+
+      if(SMS) SMS.startWatch(()=>{
+                 console.log('watching started');
+              }, Error=>{
+             console.log('failed to start watching');
+         });
+        
+        document.addEventListener('onSMSArrive', (e:any)=>{
+             var sms = e.data;
+             console.log(sms);
+      
+             });
+          
+           
         });
-
-        this.androidPermissions.requestPermissions
-        ([this.androidPermissions.PERMISSION.READ_SMS]);
-              
-        }
-        ReadSMSList()
-        {
-            
-            this.platform.ready().then((readySource) => {
-                
-            let filter = {
-            box : 'inbox', // 'inbox' (default), 'sent', 'draft'
-            indexFrom : 0, // start from index 0
-            maxCount : 3, // count of SMS to return each time
-            };
-                
-            if(SMS) SMS.listSMS(filter, (ListSms)=>{               
-            this.messages=ListSms
-            },
-            Error=>{
-              alert(JSON.stringify(Error))
-              });
-                  
-          });
-    }
-
+      
+  }
 
   signIn(phoneNumber: number){
    const appVerifier = this.recaptchaVerifier;
@@ -77,11 +61,7 @@ export class LoginPage {
     firebase.auth().signInWithPhoneNumber(phoneNumberString, appVerifier)
       .then(confirmationResult=> {
 
-      });
-      
-        
-      
-            
+      });   
   }
 
   otpVerification(otp: string, phoneNumber: number) {
